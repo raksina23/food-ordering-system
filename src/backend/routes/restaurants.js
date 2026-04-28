57baff078d45d4b3f6452fd6705cee14fbcbe69d
MadMXL <macmaruay@gmail.com>
2026-04-28 23:49:51 +0700

    setup frontend

--- src/backend/routes/restaurants.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router  = express.Router();
-
-router.get("/", (req, res) => {
-  res.json({ message: "restaurants works" });
-});
-
-module.exports = router;
